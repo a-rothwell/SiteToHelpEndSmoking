9f073906d841e3606cce4266f9115c7b8f0ec0fc
Andrew Rothwell <a_rothwell@outlook.com>
2017-04-19 23:46:57 -0400

    Added more stats

--- stats.js
+++ stats.js
@@ -1,4 +1,4 @@
 chrome.storage.sync.get(['dollarsPerYear', 'minutesPerYear'], function(value) {
-    document.getElementById("dollarsPerYear").innerHTML= "You spend " + value.dollarsPerYear + "dollars per year";
-    document.getElementById("minutesPerYear").innerHTML = "You lose" + value.minutesPerYear + "minutes per year";
+    document.getElementById("dollarsPerYear").innerHTML= "You spend " + value.dollarsPerYear + " dollars per year";
+    document.getElementById("minutesPerYear").innerHTML = "You lose " + (value.minutesPerYear / 24) + " hours of life per year";
 });
