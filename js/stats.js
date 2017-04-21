3249cfbb1cf1df875f505708469d4836d3f1a4cd
Andrew Rothwell <a_rothwell@outlook.com>
2017-04-21 13:14:25 -0400

    sound works

--- js/stats.js
+++ js/stats.js
@@ -2,3 +2,5 @@ chrome.storage.sync.get(['dollarsPerYear', 'minutesPerYear'], function(value) {
     document.getElementById("dollarsPerYear").innerHTML= "You spend " + value.dollarsPerYear + " dollars per year";
     document.getElementById("minutesPerYear").innerHTML = "You lose " + (value.minutesPerYear / 24) + " hours of life per year";
 });
+var snd = new Audio("Ticking-clock-sound.mp3"); // buffers automatically when created
+snd.play();
