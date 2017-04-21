1557e542ca48698246b2c9a4547fef88b8829d11
Andrew Rothwell <a_rothwell@outlook.com>
2017-04-20 21:39:22 -0400

    background images

--- home.js
+++ home.js
@@ -1,5 +1,17 @@
 document.getElementById("advance").addEventListener("click", myFunction);
 
-function myFunction(){
-    window.location.href='landing.html';
-}
+function myFunction() {
+    window.location.href = 'landing.html';
+};
+
+$('.count').each(function () {
+    $(this).prop('Counter',11500000).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 120000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
