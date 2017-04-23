ac3eddc94203154f4b68004909a4589de7cc0778
Andrew Rothwell <a_rothwell@outlook.com>
2017-04-22 22:40:12 -0400

    images and some text on alternitave pages

--- js/ChoicesView.js
+++ js/ChoicesView.js
@@ -1,2 +1,6 @@
 var snd = new Audio("../media/bensound-goinghigher.mp3"); // buffers automatically when created
 snd.play();
+chrome.storage.sync.get(['dollarsPerYear', 'minutesPerYear'], function(value) {
+    document.getElementById("money-spent-content").innerHTML = "Learn about better ways to spend the " + Math.ceil(value.dollarsPerYear) + " more dollars you will have if you quit smoking";
+    document.getElementById("time-spent-content").innerHTML = "Learn about better ways to spend the  " + Math.ceil((value.minutesPerYear / 60) / 24) + " more days of life you will have if you quit smoking";
+});
